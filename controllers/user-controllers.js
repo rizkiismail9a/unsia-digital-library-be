bc07ace362b1b081c6e9d22fda0cbc5146510f32
rizkiismail9a <muhamadrizkiismail9a@gmail.com>
2026-06-30 20:24:59 +0700

    feat(user): create count user endpoint

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -40,4 +40,26 @@ const getAllUsers = async (req, res, next) => {
   }
 };
 
-module.exports = { getMyProfile, getAllUsers };
+const countRegisteredUsers = async (req, res, next) => {
+  try {
+    if (req.user && req.user.role === "admin") {
+      const allUsers = await User.find({ deletedAt: null }, { password: 0 });
+
+      if (allUsers) {
+        return res.status(200).json({
+          success: true,
+          registeredUsers: allUsers.length,
+        });
+      }
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Anda tidak diizinkan menggunakan API ini",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getMyProfile, getAllUsers, countRegisteredUsers };
--- routes/api/users/users.js
+++ routes/api/users/users.js
@@ -3,10 +3,12 @@ const authMiddleware = require("../../../middleware/auth");
 const {
   getMyProfile,
   getAllUsers,
+  countRegisteredUsers,
 } = require("../../../controllers/user-controllers");
 const router = express.Router();
 
 router.get("/me", authMiddleware, getMyProfile);
+router.get("/count", authMiddleware, countRegisteredUsers);
 router.get("/", authMiddleware, getAllUsers);
 
 module.exports = router;
