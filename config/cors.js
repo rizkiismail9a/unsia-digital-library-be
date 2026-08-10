d41c54272cd51e2f73e09b43d23c9c8d4fed3a28
rizkiismail9a <muhamadrizkiismail9a@gmail.com>
2026-08-10 13:29:04 +0700

    feat: add deployed origin url in CORS whitelist

--- config/cors.js
+++ config/cors.js
@@ -1,4 +1,7 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://unsia-happy-library.netlify.app",
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
